38c07132250b53011847080921eeca5abcefd57b
marvinmahilum-cp <marvin.mahilum@coreproc.com>
2024-03-29 09:51:45 +0800

    first commit

---
+++ CollectionView-Small-Project/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView-Small-Project
+//
+//  Created by CoreProc on 3/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .red
+	}
+
+
+}
+
