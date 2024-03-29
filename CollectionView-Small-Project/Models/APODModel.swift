21a1e27040bc5bf939c47f56910f2b4d263b0ab1
marvinmahilum-cp <marvin.mahilum@coreproc.com>
2024-03-29 12:53:14 +0800

    Add models

---
+++ CollectionView-Small-Project/Models/APODModel.swift
@@ -0,0 +1,14 @@
+//
+//  APODModel.swift
+//  CollectionView-Small-Project
+//
+//  Created by CoreProc on 3/29/24.
+//
+
+import Foundation
+
+struct APODModel: Codable {
+	let title: String
+	let explanation: String
+	let url: String
+}
---
+++ CollectionView-Small-Project/Models/PhotoModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoModel.swift
+//  CollectionView-Small-Project
+//
+//  Created by CoreProc on 3/29/24.
+//
+
+import Foundation
+
+struct PhotoModel: Codable {
+	let id: Int
+	let imgSrc: String
+}
